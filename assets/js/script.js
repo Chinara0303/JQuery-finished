138096842ba64c4e8a0c148729378229297d3b61
Chinara0303 <chinaraei@code.edu.az>
2023-02-28 18:50:32 +0400

    Added slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,33 @@ $(document).ready(function () {
     let surnameInp = $(".surname-inp");
     let users = [];
 
-    if (JSON.parse(localStorage.getItem("users")) != null) {
+    function openModal() {
+        $(".open-btn").click(function () {
+            $(".body-modal").slideDown();
+            $(".body-modal").removeClass("d-none");
+            $(".overlay").css("display", "block")
+            $("ul").css("z-index", "-1")
+            $(".slider").css("z-index", "-1")
+
+        })
+    }
+    openModal();
+    closeModal();
+
+    function closeModal() {
+        $(".close-btn").click(function () {
+            $(".body-modal").slideUp();
+            $(".body-modal").addClass("d-none");
+            $(".overlay").css("display", "none")
+            $("ul").css("z-index", "1")
+            $(".slider").css("z-index", "1")
+        })
+    }
+    if (localStorage.getItem("users") != null) {
         users = JSON.parse(localStorage.getItem("users"));
     }
 
-    $(document).on("click", "button", function () {
+    $(".add-btn").click(function () {
         $("ul").html("");
         users.push({
             id: uuidv4(),
@@ -16,8 +38,11 @@ $(document).ready(function () {
         })
 
         localStorage.setItem("users", JSON.stringify(users))
-        addUi()
-
+        addUi();
+        $(".body-modal").slideUp();
+        $(".body-modal").addClass("d-none");
+        $(".overlay").css("display", "none");
+        $("ul").css("z-index", "1")
     })
 
     addUi();
@@ -29,12 +54,13 @@ $(document).ready(function () {
     function addUi() {
         for (const dbUser of users) {
             $("ul").append(`
-             <li data-id="${dbUser.id}" class="list-group-item item">${dbUser.name} ${dbUser.surname}</li>
+             <li data-id="${dbUser.id}" class="list-group-item">${dbUser.name} ${dbUser.surname} <i class="fa-solid fa-trash-can item"></i></li>
             `);
             nameInp.val("")
             surnameInp.val("")
         }
     }
+
     function delItems(id) {
         let existUser = users.filter(u => u.id != id);
         users = existUser;
@@ -42,14 +68,65 @@ $(document).ready(function () {
     }
 
     $(document).on("click", ".item", function () {
-        for (const user of users) {
-            if (user.id == $(this).attr("data-id")) {
-                delItems(user.id);
-
-                $(this).remove()
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                for (const user of users) {
+                    if (user.id == $(this).parent().attr("data-id")) {
+                        delItems(user.id);
+                        $(this).parent().remove()
+                    }
+                }
+                Swal.fire(
+                    'Deleted!',
+                    'Your data has been deleted.',
+                    'success'
+                )
             }
-        }
+        })
+
     })
+
+
+    //slider
+    function rigthSlider() {
+       
+            let activeSlider = $(".active-slider");
+            if (activeSlider.next().length != 0) {
+                activeSlider.removeClass("active-slider");
+                activeSlider.next().addClass("active-slider");
+            }
+            else {
+                activeSlider.removeClass("active-slider");
+                activeSlider.parent().children().eq(0).addClass("active-slider");
+            }
+
+    }
+    function leftSlider() {
+        let activeSlider = $(".active-slider");
+        if (activeSlider.prev().length != 0) {
+            activeSlider.removeClass("active-slider");
+            activeSlider.prev().addClass("active-slider");
+        }
+        else {
+            activeSlider.removeClass("active-slider");
+            activeSlider.parent().children().eq(2).addClass("active-slider");
+        }
+    }
+
+   $(document).on("click", ".left", leftSlider) 
+   $(document).on("click", ".right", rigthSlider)
+   $(document).on("mouseover", ".left", leftSlider) 
+   $(document).on("mouseover", ".right", rigthSlider)
+
+   setInterval(() => {
+    rigthSlider()
+   }, 2000);
 })
-
-
